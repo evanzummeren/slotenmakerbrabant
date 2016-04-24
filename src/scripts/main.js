efa8b957d4e194b13290b1f5ab4a545f7d46deda
evanzummeren <erikvanzummeren@gmail.com>
2016-04-24 14:13:02 +0200

    add animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -50,6 +50,75 @@ tl.to($circle, .5, {
 	delay: 1
 });
 
+$first = $('#first');
+$second = $('#second');
+$third = $('#third');
+$fourth = $('#fourth');
+$fifth = $('#fifth');
+
+var tlFirst = new TimelineMax({repeat:100, delay: 0});
+var tlSecond = new TimelineMax({repeat:100, delay: 0});
+var tlThird = new TimelineMax({repeat:100, delay: 0});
+var tlFourth = new TimelineMax({repeat:100, delay: 0});
+var tlFifth = new TimelineMax({repeat:100, delay: 0});
+
+
+tlFirst.to($first, 4, {
+	x: -10,
+	y: -10,
+	ease: Power2.easeInOut
+})
+tlFirst.to($first, 3, {
+	x: 0,
+	y: 10,
+	scale: 0.95,
+	ease: Power2.easeIn
+})
+tlFirst.to($first, 2, {
+	x: 0,
+	y: 0,
+	scale: 1,
+	ease: Power2.easeInOut
+})
+
+tlSecond.to($second, 4, {
+	x: -10,
+	ease: Power2.easeInOut
+})
+tlSecond.to($second, 3, {
+	x: 0,
+	ease: Power2.easeIn
+})
+
+tlThird.to($third, 4, {
+	y: -10,
+	ease: Power2.easeInOut
+})
+tlThird.to($third, 3, {
+	y: 0,
+	ease: Power2.easeIn
+})
+
+tlFourth.to($fourth, 4, {
+	x: 10,
+	y: 10,
+	ease: Power2.easeInOut
+})
+tlFourth.to($fourth, 5, {
+	x: 0,
+	y: 0,
+	ease: Power2.easeIn
+})
+
+tlFifth.to($fifth, 4, {
+	x: 10,
+	ease: Power2.easeInOut
+})
+tlFifth.to($fifth, 5, {
+	x: 0,
+	ease: Power2.easeIn
+})
+
 /* Analytics */
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-24843696-5']);
