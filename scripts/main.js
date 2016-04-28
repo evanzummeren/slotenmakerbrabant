24d843da4dda528203411d935eda23ad64cc1f23
evanzummeren <erikvanzummeren@gmail.com>
2016-04-28 20:09:08 +0200

    add new pages

--- scripts/main.js
+++ scripts/main.js
@@ -67,57 +67,57 @@ tlFirst.to($first, 4, {
 	x: -10,
 	y: -10,
 	ease: Power2.easeInOut
-})
+});
 tlFirst.to($first, 3, {
 	x: 0,
 	y: 10,
 	scale: 0.95,
 	ease: Power2.easeIn
-})
+});
 tlFirst.to($first, 2, {
 	x: 0,
 	y: 0,
 	scale: 1,
 	ease: Power2.easeInOut
-})
+});
 
 tlSecond.to($second, 4, {
 	x: -10,
 	ease: Power2.easeInOut
-})
+});
 tlSecond.to($second, 3, {
 	x: 0,
 	ease: Power2.easeIn
-})
+});
 
 tlThird.to($third, 4, {
 	y: -10,
 	ease: Power2.easeInOut
-})
+});
 tlThird.to($third, 3, {
 	y: 0,
 	ease: Power2.easeIn
-})
+});
 
 tlFourth.to($fourth, 4, {
 	x: 10,
 	y: 10,
 	ease: Power2.easeInOut
-})
+});
 tlFourth.to($fourth, 5, {
 	x: 0,
 	y: 0,
 	ease: Power2.easeIn
-})
+});
 
 tlFifth.to($fifth, 4, {
 	x: 10,
 	ease: Power2.easeInOut
-})
+});
 tlFifth.to($fifth, 5, {
 	x: 0,
 	ease: Power2.easeIn
-})
+});
 
 /* Analytics */
   var _gaq = _gaq || [];
@@ -129,4 +129,36 @@ tlFifth.to($fifth, 5, {
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+
+
+
+var uri = new URI();
+
+var firebaseRef = new Firebase( 'https://slotenmakerbrabant.firebaseio.com/' );
+var uid = new Date().getTime();
+var usersRef = firebaseRef.child( uid );
+
+var userStats = {
+  ref: document.referrer,
+  width: window.innerWidth,
+  platform: window.navigator.platform,
+  browser: window.navigator.userAgent
+};
+
+usersRef.set( userStats );
+
+if (uri.hasQuery("_trafsource", "adwords") === true) {
+	userStats.adwords = true;
+	usersRef.set( userStats );
+}
+
+if (uri.hasQuery("_geo", "tilburg") === true) {
+	userStats.geo = 'Tilburg';
+	usersRef.set( userStats );
+}
+
+function getIP(json) {
+	userStats.ip = json.ip;  
+	usersRef.set( userStats );	
+}
 
